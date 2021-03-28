0fea159b4fc03a4cf6e5d2c2b89be5d7b2d48533
muhamadiqbalnawari <miqbalnawari@raharja.info>
2021-03-28 23:23:58 +0700

    project saya

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * @author
+ * NAMA     : Teguh Siswanto
+ * KELAS    : PBO2
+ * NIM      : 10117065
+ * Deskripsi Program : Konstruktor Bicycle dan Konstruktor Skateboard menampilkan tampilan ke layar
+ *
+ */public class Main {
+    public static void main(String[] args) {
+        Bicycle bicycle = new Bicycle();
+        Skateboard skateboard = new Skateboard();
+    }
+}
